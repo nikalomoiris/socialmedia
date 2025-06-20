b4ffbcdbdb5dcf284c89ae1c9a4b96423c1d2516
Nik Kalomoiris <nikalomoiris@gmail.com>
2025-06-20 16:42:00 -0400

    Remove unused RestController import from MainController

--- src/main/java/com/nik/socialmedia/controller/MainController.java
+++ src/main/java/com/nik/socialmedia/controller/MainController.java
@@ -4,7 +4,6 @@ package com.nik.socialmedia.controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @CrossOrigin
